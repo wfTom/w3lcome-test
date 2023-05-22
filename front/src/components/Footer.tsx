c231b80da0db610e4871387aa73c70ab3736fed5
Tom <wf_tom@hotmail.com.br>
2023-05-21 22:58:24 -0300

    Created Footer component, alter layout and index page

---
+++ front/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export function Footer() {
+  return (
+    <div className="footer text-sm leading-relaxed text-gray-200">
+      Feito com &#10084; por{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href=""
+      >
+        Tom
+      </a>
+    </div>
+  )
+}
